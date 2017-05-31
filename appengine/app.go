5e8e6d7606bbd80cc02a0b3154c6188442b67ae4
nirasan <niranirao@gmail.com>
2017-05-31 16:44:15 +0900

    minimam app

---
+++ appengine/app.go
@@ -0,0 +1,11 @@
+package appengine
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello World! Hellow World!"))
+	})
+}
